3176a84c9ec65a9f9d6aed3617d212cbe69fe031
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-04-20 22:20:09 +0530

    Added DrawingView template File!

--- app/src/main/java/com/broood/drawingapp/DrawingView.kt
+++ app/src/main/java/com/broood/drawingapp/DrawingView.kt
@@ -1,5 +1,41 @@
 package com.broood.drawingapp
 
-class DrawingView {
-    
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var mColor = Color.BLACK
+    private var mCanvas: Canvas? = null
+
+    init{
+        setUpDrawing()
+    }
+    fun setUpDrawing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(mColor, mBrushSize)
+        mDrawPaint!!.color = mColor
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+
+
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float): android.graphics.Path() {
+
+
+    }
 }
+
