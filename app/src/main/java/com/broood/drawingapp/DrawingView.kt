d7061a2d639519ad68de0178b7a90dd117836555
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-04-22 10:46:15 +0530

    Can now draw on the screen, and the lines will last!

--- app/src/main/java/com/broood/drawingapp/DrawingView.kt
+++ app/src/main/java/com/broood/drawingapp/DrawingView.kt
@@ -17,6 +17,8 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.toFloat()
     private var mColor = Color.BLACK
     private var mCanvas: Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
+
 
     init{
         setUpDrawing()
@@ -43,6 +45,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas){
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if(!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -61,6 +70,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 mDrawPath!!.moveTo(touchX!!, touchY!!)
             }
             MotionEvent.ACTION_MOVE ->{
+                mPaths.add(mDrawPath!!)
                 mDrawPath!!.lineTo(touchX!!, touchY!!)
             }
             MotionEvent.ACTION_UP -> {
