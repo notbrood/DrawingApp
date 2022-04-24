94ed6e86857cc33e582edcd94d158fff3dd606cc
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-04-24 17:30:53 +0530

    Added Brush dialog stuff!

---
+++ app/src/main/java/com/broood/drawingapp/dialog_brush_size.kt
@@ -0,0 +1,11 @@
+package com.broood.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dialog_brush_size : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog_brush_size)
+    }
+}
