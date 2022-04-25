f77a00a719d757ad1aa22f9489eedbadc92d50dc
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-04-25 07:04:31 +0530

    Added Brush size changing dialogue!

--- app/src/main/java/com/broood/drawingapp/MainActivity.kt
+++ app/src/main/java/com/broood/drawingapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.broood.drawingapp
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
@@ -11,8 +15,32 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawingView)
         drawingView?.setSizeForBrush(20.toFloat())
 
-    }
-    private fun showBrushSizeChooserDialog(){
+        val ib_brush: ImageButton = findViewById<ImageButton>(R.id.ib_brush)
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
 
+
+    }
+    private fun showBrushSizeChooserDialog() {
+        var brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.activity_dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
--- app/src/main/java/com/broood/drawingapp/dialog_brush_size.kt
+++ app/src/main/java/com/broood/drawingapp/dialog_brush_size.kt
@@ -2,10 +2,12 @@ package com.broood.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class dialog_brush_size : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog_brush_size)
+
     }
 }
