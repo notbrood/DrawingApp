4d918663d22903b617aeba5594663539171d0de3
Aman Jain <aman.jain2021@vitbhopal.ac.in>
2022-05-15 01:36:30 +0530

    Added gallery button code!

--- app/src/main/java/com/broood/drawingapp/MainActivity.kt
+++ app/src/main/java/com/broood/drawingapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.broood.drawingapp
 import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.provider.MediaStore
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -16,6 +19,13 @@ import androidx.core.app.ActivityCompat
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    val openGalleryLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == RESULT_OK && result.data!=null){
+            val imageBackGround:ImageView = findViewById(R.id.iv_background)
+            imageBackGround.setImageURI(result.data?.data)
+        }
+    }
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
             permissions ->
@@ -24,11 +34,17 @@ class MainActivity : AppCompatActivity() {
                 val isGranted = it.value
 
                 if(isGranted){
-                    Toast.makeText(this@MainActivity, "Permission granted, now you can read storage files", Toast.LENGTH_LONG).show()
+                    if(permissionName == Manifest.permission.WRITE_EXTERNAL_STORAGE)(Toast.makeText(this@MainActivity, "Permission granted, now you can write storage Files", Toast.LENGTH_LONG).show())
+                    else(Toast.makeText(this@MainActivity, "Permission granted, now you can read storage files", Toast.LENGTH_LONG).show())
+                    val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
                 }
                 else{
                     if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(this@MainActivity, "YOU DENIED NIGGA", Toast.LENGTH_LONG).show()
+                    }
+                    if(permissionName == Manifest.permission.WRITE_EXTERNAL_STORAGE){
+                        Toast.makeText(this@MainActivity, "YOU DENIED write NIGGA", Toast.LENGTH_LONG).show()
                     }
                 }
             }
@@ -107,7 +123,16 @@ class MainActivity : AppCompatActivity() {
         else{
             requestPermission.launch(arrayOf(
                 Manifest.permission.READ_EXTERNAL_STORAGE
-            // TODO - Add writing storage permission
+            ))
+        }
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )){
+            showRationalDialog("Kids Drawing App", "Kids Drawing App " + "write needs to Access your Storage")
+        }
+        else{
+            requestPermission.launch(arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ))
         }
     }
